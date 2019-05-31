0f2f70cf5eddbe105955966121b9f0888e04fa60
xinyashan <50379822+xinyashan@users.noreply.github.com>
2019-05-31 15:08:56 -0500

    Add files via upload

---
+++ newhomework-fruit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HWFRUIT
+{
+
+
+    public class ninjafruit
+    {
+
+        public string _ninjafruit { set; get; }
+        public string _holyninjamagic { set; get; }
+
+
+        public ninjafruit(string ninjafruit, string holyninjamagic)
+        {
+            _ninjafruit = ninjafruit;
+            _holyninjamagic = holyninjamagic;
+            Console.WriteLine("Use your ninja powers to continue this mission! Remember each fruit represents different power!");
+
+
+
+        }
+        public void usemagic(int magicpowerused)
+        {
+            for (int i = 0; i < magicpowerused; i++)
+            {
+                Console.WriteLine("The user has used {0}");
+                Console.WriteLine("Warning! If you use this magical power move your health will be damaged too! It is about {0}!", _holyninjamagic);
+
+
+            }
+
+
+
+        }
+
+        // The end of the usepower method!!!! 
+
+
+    }
+}
